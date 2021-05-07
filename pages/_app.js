db4af2f2857fe6258089c92b521a959949f2c2b7
DevOM <oplondhe@gmail.com>
2021-05-07 14:26:02 +0530

    After modifying Social Card Description

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps, router }) {
           content="A platform exclusively for 'GPA'"
           key="type"
         />
-        <meta property="og:title" content="GPAForum | Gand" key="title" />
+        <meta property="og:title" content="GPAForum" key="title" />
         <meta
           name="description"
           content="Sign Up to be the indivisible part of GPA forever."
